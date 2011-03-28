fde650cf44e681c2f128191b680cadbd3eee14f6
Chris Killpack <killpack@squareup.com>
2011-03-27 17:35:41 -0700

    Completed to end of about_triangles koan.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  triangle = :isosceles
+  if a == b && b == c
+    triangle = :equilateral
+  else
+    triangle = :scalene if a != b && b != c && a != c
+  end
+  triangle
 end
 
 # Error class used in part 2.  No need to change this code.
