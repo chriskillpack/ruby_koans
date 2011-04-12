b7e355f9ddc210915ad06308a02a4f13149f57b7
Chris Killpack <killpack@squareup.com>
2011-04-12 13:13:45 -0700

    Completed to end of modules koan.

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,17 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def roll(count)
+    @values = []
+    count.times do
+      @values << rand(6) + 1
+    end
+  end
+end
+
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
